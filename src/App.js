259694a4c293fd960b477ff17b68934c8917cdfe
swarnim adarsh <swarnimadarsh.3579@gmail.com>
2023-10-15 20:45:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Table from './Components/Table';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "pt-5" id = "bg">
+      <h1 className = "text-center">User Records</h1>
+      <Table />
     </div>
   );
 }
 
-export default App;
+export default App; 
